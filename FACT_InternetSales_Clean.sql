468e6e1577d96450ffee451cde5c266e9a98edd0
Aji Prasetyo <117008370+ajiprasetyo11@users.noreply.github.com>
2024-08-18 16:39:45 +0700

    Add files via upload

---
+++ FACT_InternetSales_Clean.sql
@@ -0,0 +1,16 @@
+/* QUESTION 4 */
+-- Cleansed FACT_InternetSales Table
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  [SalesOrderNumber],  
+  [SalesAmount]
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2
+ORDER BY
+  OrderDateKey ASC
